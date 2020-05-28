e24f0082956c71bb70deed45859fdb42922db25e
Sam Jones <samakj@live.co.uk>
2020-05-28 11:54:24 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="flagon",
+    version="0.1",
+    description="A small flask wrapper",
+    url="http://github.com/samakj/flagon",
+    author="Sam Jones",
+    author_email="samakj@live.co.uk",
+    license="MIT",
+    packages=["flagon"],
+    install_requires=["flask", "flask_caching"],
+    zip_safe=False
+)
